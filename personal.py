26315e1400a0a0edd262288dd953d1e52fba56a1
VinzeG <124074279+VinzeG@users.noreply.github.com>
2024-11-22 12:14:15 -0400

    Update personal.py

--- personal.py
+++ personal.py
@@ -26,3 +26,30 @@ class CPersonal:
             return mi_result
         except ValueError as error:
             print("Error al mostrar datos{}".format(error))
+
+    def Eliminar_personal(id_usu):
+        try:
+            connect = CConexion.Conexion_BDD()
+            cursor = connect.cursor()
+            sql =("Delete from usuario where usuario.id_usu = %s;")
+            valores = (id_usu,)
+            cursor.execute(sql,valores)
+            connect.commit()
+            print(cursor.rowcount,"Registro eliminado")
+            connect.close
+
+        except ValueError as error:
+            print("Error al eliminar registro{}",format(error))
+
+    def Modif_personal(id_usu,nom_usu,apell_usu,cargo_usu,sexo_usu):
+        try:
+            connect = CConexion.Conexion_BDD()
+            cursor = connect.cursor()
+            sql = ("update usuario set usuario.nom_usu = %s, usuario.apell_usu = %s,  usuario.cargo_usu = %s, usuario.sexo_usu = %s where usuario.id_usu = %s;")
+            valores = (nom_usu,apell_usu,cargo_usu,sexo_usu,id_usu)
+            cursor.execute(sql,valores)
+            connect.commit()
+            print(cursor.rowcount,"Registro actualizado")
+            connect.close
+        except ValueError as error:
+            print("Error al eliminar registro{}".format(error))
